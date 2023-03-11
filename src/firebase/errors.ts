89defeb275887695643058eb73c48c6d14dfc870
HCVincent <lilacsweet@163.com>
2023-03-11 21:33:48 +0800

    Sign in Firebase

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,4 +1,6 @@
 export const FIREBASE_ERRORS = {
     "Firebase: Error (auth/email-already-in-use).": "A user with that email already exists.",
-    "Firebase: Password should be at least 6 characters (auth/weak-password).": "Password should be at least 6 characters."
+    "Firebase: Password should be at least 6 characters (auth/weak-password).": "Password should be at least 6 characters.",
+    "Firebase: Error (auth/wrong-password).": "wrong password",
+    "Firebase: Error (auth/user-not-found).": "email not found",
 }
