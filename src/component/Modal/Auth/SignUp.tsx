3d2bcad08257cb1619c28f5de18060c2fc398825
HCVincent <lilacsweet@163.com>
2023-03-11 16:36:34 +0800

    firebase errors handling

--- src/component/Modal/Auth/SignUp.tsx
+++ src/component/Modal/Auth/SignUp.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
 
 const SignUp: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -99,9 +100,10 @@ const SignUp: React.FC = () => {
         }}
         bg="gray.50"
       />
-      {error && (
+      {(error || userError) && (
         <Text textAlign="center" color="red" fontSize="10pt">
-          {error}
+          {error ||
+            FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
         </Text>
       )}
       <Button
